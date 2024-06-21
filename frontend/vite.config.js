46fb65aab241411c15b905369f90453a05d120f6
abhishek2001panwar <abhishek.panwar.cseiot.2022@miet.ac.in>
2024-06-21 19:27:02 +0530

    refactor: update frontend proxy configuration to use the deployed API URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-     "/api": "http://localhost:4000",
+     "/api": "https://communify-indol.vercel.app/",
     },
   },
   plugins: [react()],
