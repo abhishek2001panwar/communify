905c5bd3d43f2f2d18ee49f7b981a3aeb1651bab
abhishek2001panwar <abhishek.panwar.cseiot.2022@miet.ac.in>
2024-06-21 19:37:25 +0530

    refactor: update frontend proxy configuration to use the deployed API URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,28 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//      "/api": "https://communify-indol.vercel.app",
+//     },
+//   },
+//   plugins: [react()],
+// });
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-     "/api": "https://communify-indol.vercel.app",
+      '/api': {
+        target: 'https://communify-indol.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
     },
   },
   plugins: [react()],
